9b089a598b8eeaea9c3c3daa0267cc564a98fd28
Yan Martins <yanmartinss05@gmail.com>
2024-11-08 12:09:21 -0300

    pagina de login

--- code-and-shave-front/src/app/layout.jsx
+++ code-and-shave-front/src/app/layout.jsx
@@ -7,7 +7,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className="bg-gray-950 text-white">
+      <body className="bg-gray-950 text-black">
         {children}
       </body>
     </html>
--- code-and-shave-front/src/pages/Login.jsx
+++ code-and-shave-front/src/pages/Login.jsx
@@ -1,40 +1,62 @@
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { useState } from 'react';
 
 export const Login = () => {
-    return (
-        <div className='flex justify-center bg-[#0F1111] min-h-screen'>
-            <div className='flex flex-col w-screen md:flex-row'>
-                <div>
-                    <div className='bg-[url("../assets/images/photo-login.jpg")] bg-cover bg-no-repeat bg-center h-72 md:w-72 md:h-full'></div>
-                </div>
 
-                <div className='bg-white flex flex-col items-center p-5 flex-1 gap-3 justify-center'>
-                    <div className='flex flex-col justify-center gap-3 text-center'>
-                        <h1 className='text-black font-bold text-3xl'>Code & Shave 💈</h1>
-                        <p className='text-gray-600 md:w-80'>Sistema de agendamento fácil e rápido para barbearias e clientes.</p>
-                    </div>
-                    <div className='max-w-xs'>
-                        <label className='text-gray-600 font-bold'>Email</label>
-                        <input type="email"
-                        className='shadow-sm outline-none p-1 text-gray-600 w-full rounded-md' />
-                    </div>
-                    <div className='max-w-xs'>
-                        <label className='text-gray-600 font-bold'>Senha</label>
-                        <div className='shadow-sm flex justify-between items-center p-1'>
-                            <input type="password"
-                            className='bg-transparent text-gray-600 rounded-md outline-none' />
-                            <VisibilityIcon sx={{color: 'gray'}} />
+    const [typePassword, setTypePassword] = useState('password');
+
+    const switchTypePassword = (e) => {
+        e.preventDefault();
+        if (typePassword === 'password') {
+            setTypePassword('text');
+        } else {
+            setTypePassword('password');
+        }
+    }
+
+    return (
+        <div className='bg-[#24211c] min-h-screen w-screen flex justify-center items-center p-3 bg-gradient-to-b from-black/90 to-black/40'>
+            <div className='bg-white w-[390px] h-[600px] rounded-md overflow-hidden shadow-sm md:w-[95vw] flex flex-col md:flex-row lg:flex-row-reverse justify-start lg:w-[900px] transition-all'>
+                <div className='bg-[url("../assets/images/photo-login.jpg")] h-[230px] bg-cover flex justify-center items-center md:w-[40%] md:h-[100%] bg-center lg:w-[45%]'></div>
+
+                <div className='p-3 flex flex-col justify-center items-center text-center md:w-[60%] lg:w-[55%]'>
+                    <h2 className='text-2xl font-bold mb-1 md:text-2xl lg:text-3xl'>Code & Shave 💈</h2>
+                    <p className='text-gray-600 mb-1 max-w-[300px] text-sm md:text-base'>Sistema de agendamento fácil e rápido para barbearias e clientes.</p>
+                    <h1 className='font-bold text-lg md:text-xl lg:text-2xl mt-3'>Acesse sua conta</h1>
+
+                    <form className='mt-5 flex flex-col justify-center gap-3'>
+                        <div className='flex flex-col'>
+                            <label className='text-left text-gray-500 text-sm'>Email</label>
+                            <input type="email"
+                            autoComplete='email'
+                            placeholder='Digite seu email'
+                            className='outline-none shadow-lg rounded-md p-2 text-gray-500 w-full' />
                         </div>
-                    </div>
-                    <div className='flex max-w-xs'>
-                        <input type="submit"
-                        value="Login"
-                        className='bg-blue-600 text-white px-8 py-3 rounded-md font-bold' />
-                    </div>
-                    <div className='flex flex-col justify-center items-center gap-1'>
-                        <p className='text-gray-500 cursor-pointer'>Cadastrar-se como Cliente</p>
-                        <p className='text-gray-500 cursor-pointer'>Cadastrar-se como Barbearia</p>
-                    </div>
+
+                        <div className='flex flex-col'>
+                            <label className='text-left text-gray-500 text-sm rounded-md'>Senha</label>
+                            <div className='shadow-lg p-2 w-full'>
+                                <input type={typePassword}
+                                placeholder='Digite sua senha'
+                                autoComplete='password'
+                                className='outline-none bg-transparent text-gray-500' />
+                                <button onClick={e => switchTypePassword(e)}>
+                                    {typePassword === 'password' ? <VisibilityIcon sx={{color: '#6B7280'}} /> : <VisibilityOffIcon  sx={{color: '#6B7280'}} />}
+                                </button>
+                            </div>
+                        </div>
+
+                        <div>
+                            <input type="submit" value="Login"
+                            className='bg-blue-500 text-white px-7 py-2 rounded-md cursor-pointer' />
+                        </div>
+
+                        <div className='flex flex-col gap-1'>
+                            <p className='text-gray-500 underline cursor-pointer'>Cadastrar-se como cliente</p>
+                            <p className='text-gray-500 underline cursor-pointer'>Cadastrar-se como barbearia</p>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
