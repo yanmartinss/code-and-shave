8fdd11b65621290b6257a1b6020953b841199821
Yan Martins <137118093+yanmartinss@users.noreply.github.com>
2024-11-21 17:35:47 -0300

    tentando pegar a localização do usuario

--- code-and-shave-front/src/pages/TelasCliente/HomeCliente.js
+++ code-and-shave-front/src/pages/TelasCliente/HomeCliente.js
@@ -1,7 +1,28 @@
 import GpsFixedIcon from '@mui/icons-material/GpsFixed';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
+import { useAuth } from '../../contexts/AuthContext';
 
 export const HomeCliente = () => {
+
+    const {userLogged} = useAuth();
+    const [barberInput, setBarberInput] = useState();
+    const [localInput, setLocalInput] = useState();
+    const [userLocation, setUserLocation] = useState(null);
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const { latitude, longitude } = position.coords;
+                setUserLocation({ latitude, longitude });
+                // {fetchBarbeariasByLocation(latitude, longitude); // Buscar barbearias próximas}
+                console.log(`${latitude}, ${longitude}`)
+            });
+        } else {
+            alert("Digite seu endereço por favor.");
+        }
+    };
+
     return (
         <div>
             <div className='grid gap-5 w-full'>
@@ -10,7 +31,9 @@ export const HomeCliente = () => {
                         <input
                         className="outline-none bg-transparent text-white/90 placeholder:text-white/90 flex-1"
                         type="text"
-                        placeholder="Encontre sua barbearia favorita"/>
+                        placeholder="Encontre sua barbearia favorita"
+                        value={barberInput}
+                        onChange={e => setBarberInput(e.target.value)} />
                         <button><SearchIcon sx={{color: '#e0dfdf'}} /></button>
                     </div>
                 
@@ -18,8 +41,10 @@ export const HomeCliente = () => {
                         <input
                         className="outline-none bg-transparent text-white/90 placeholder:text-white/90 flex-1"
                         type="text"
-                        placeholder="Onde você está?"/>
-                        <button><GpsFixedIcon sx={{color: '#e0dfdf'}} /></button>
+                        placeholder="Onde você está?"
+                        value={localInput}
+                        onChange={e => setLocalInput(e.target.value)} />
+                        <button onClick={getLocation}><GpsFixedIcon sx={{color: '#e0dfdf'}} /></button>
                     </div>
                 </div>
 
