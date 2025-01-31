5cee78d8a915482670199df2846e2a28475b8dcb
Yan Martins <yanmartinss05@gmail.com>
2025-01-31 10:12:10 -0300

    sasa

--- code-and-shave-back/src/main/java/br/com/code_and_shave/repositories/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.code_and_shave.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.com.code_and_shave.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
